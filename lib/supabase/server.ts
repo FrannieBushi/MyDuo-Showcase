ac4e524d53c86616738170ea3c8b10fce18fe49c
Fran Perez <molinapfranj@gmail.com>
2025-09-16 13:11:16 +0200

    feat setup supebase && authentication

---
+++ lib/supabase/server.ts
@@ -0,0 +1,40 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+
+    const cookieStore = await cookies();
+
+    return createServerClient(
+        
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+       
+        {
+            cookies: {
+                
+                getAll() {
+
+                    return cookieStore.getAll();
+                },
+                setAll(cookiesToSet) {
+                    
+                    try {
+                        cookiesToSet.forEach(({name, value, options}) =>
+
+                            cookieStore.set(name, value, options)
+
+                        );
+
+                    } catch {
+                        // El método setAll es llamado por un componente server
+                        //Esto puede ser ignorado si  tienes un middleware refrescando
+                        //Sesiones de usuario
+                    }
+                },
+            },  
+        },
+    )
+}
+
+
